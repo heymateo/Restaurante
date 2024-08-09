52bc2b53d52017e99e8e036e33bd05d74f47aa1c
Crizzi-11 <cmarin90677@ufide.ac.cr>
2024-08-09 12:04:59 -0600

    CreacionDeCuenta

--- frontend/frontend/Controllers/CuentaController.cs
+++ frontend/frontend/Controllers/CuentaController.cs
@@ -11,146 +11,30 @@ namespace frontend.Controllers
 {
     public class CuentaController : Controller
     {
-        private readonly FrontendDbContext _context;
-
-        public CuentaController(FrontendDbContext context)
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CuentaController(IHttpClientFactory httpClientFactory)
         {
-            _context = context;
+            _httpClientFactory = httpClientFactory;
         }
 
-        // GET: Cuenta
+        // Método para listar administradores
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Cuenta.ToListAsync());
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7061/api/Cuenta");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var administradores = await response.Content.ReadFromJsonAsync<List<CuentaModel>>();
+                return View(administradores);
+            }
+            else
+            {
+                // Manejar el error
+                return View(new List<CuentaModel>());
+            }
         }
 
-        // GET: Cuenta/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cuentaModel = await _context.Cuenta
-                .FirstOrDefaultAsync(m => m.Id_Cuenta == id);
-            if (cuentaModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(cuentaModel);
-        }
-
-        // GET: Cuenta/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Cuenta/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id_Cuenta,Id_Cliente,Id_Orden,Total,Cancelado")] CuentaModel cuentaModel)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(cuentaModel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cuentaModel);
-        }
-
-        // GET: Cuenta/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cuentaModel = await _context.Cuenta.FindAsync(id);
-            if (cuentaModel == null)
-            {
-                return NotFound();
-            }
-            return View(cuentaModel);
-        }
-
-        // POST: Cuenta/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id_Cuenta,Id_Cliente,Id_Orden,Total,Cancelado")] CuentaModel cuentaModel)
-        {
-            if (id != cuentaModel.Id_Cuenta)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cuentaModel);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CuentaModelExists(cuentaModel.Id_Cuenta))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(cuentaModel);
-        }
-
-        // GET: Cuenta/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cuentaModel = await _context.Cuenta
-                .FirstOrDefaultAsync(m => m.Id_Cuenta == id);
-            if (cuentaModel == null)
-            {
-                return NotFound();
-            }
-
-            return View(cuentaModel);
-        }
-
-        // POST: Cuenta/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var cuentaModel = await _context.Cuenta.FindAsync(id);
-            if (cuentaModel != null)
-            {
-                _context.Cuenta.Remove(cuentaModel);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CuentaModelExists(int id)
-        {
-            return _context.Cuenta.Any(e => e.Id_Cuenta == id);
-        }
+        
     }
 }
