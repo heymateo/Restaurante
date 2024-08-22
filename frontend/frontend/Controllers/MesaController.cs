7b76d7cc69485efd1f2a4a765379a5b7039743e5
JerryC19 <102439415+JerryC19@users.noreply.github.com>
2024-08-21 23:45:32 -0600

    Mesa Controllers y Vistas
    Se añadio en controller Mesa y sus vista respectivas

---
+++ frontend/frontend/Controllers/MesaController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace frontend.Controllers
+{
+    public class MesaController : Controller
+    {
+        private readonly BackendDbContext _context;
+
+        public MesaController(BackendDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Mesa
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Mesa.ToListAsync());
+        }
+
+        // GET: Mesa/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mesa = await _context.Mesa
+                .FirstOrDefaultAsync(m => m.Id_Mesa == id);
+            if (mesa == null)
+            {
+                return NotFound();
+            }
+
+            return View(mesa);
+        }
+
+        // GET: Mesa/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mesa/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id_Mesa,Numero_Mesa,Disponible,Activa")] Mesa mesa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mesa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mesa);
+        }
+
+        // GET: Mesa/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mesa = await _context.Mesa.FindAsync(id);
+            if (mesa == null)
+            {
+                return NotFound();
+            }
+            return View(mesa);
+        }
+
+        // POST: Mesa/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id_Mesa,Numero_Mesa,Disponible,Activa")] Mesa mesa)
+        {
+            if (id != mesa.Id_Mesa)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mesa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MesaExists(mesa.Id_Mesa))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mesa);
+        }
+
+        // GET: Mesa/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mesa = await _context.Mesa
+                .FirstOrDefaultAsync(m => m.Id_Mesa == id);
+            if (mesa == null)
+            {
+                return NotFound();
+            }
+
+            return View(mesa);
+        }
+
+        // POST: Mesa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mesa = await _context.Mesa.FindAsync(id);
+            if (mesa != null)
+            {
+                _context.Mesa.Remove(mesa);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MesaExists(int id)
+        {
+            return _context.Mesa.Any(e => e.Id_Mesa == id);
+        }
+    }
+}
